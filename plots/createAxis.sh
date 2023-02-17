d6083156335c262a80f949910d6620a5ae97a135
ByteHamster <info@bytehamster.com>
2023-02-17 16:06:39 +0100

    Nicer plots

--- plots/createAxis.sh
+++ plots/createAxis.sh
@@ -2,13 +2,15 @@
 
 function writePlots() {
     dataset=$1
+    additional=$2
     cat <<EOF >> construction.tex
 \\begin{tikzpicture}
     \\begin{axis}[
         title={$dataset},
         plotCompetitorConstruction,
-        legend to name=legendCompetitors,
+        legend to name=legendCompetitors,$additional
       ]
+      \\addplot[no marks,veryLightGrey,line width=2pt,forget plot,sharp plot] coordinates { (13,0) (13,100) };
       %% MULTIPLOT(name|ptitle|attr)
       %%   SELECT
       %%     MIN(bitsPerElement, 13) as x,
@@ -28,8 +30,9 @@ EOF
     \\begin{axis}[
         title={$dataset},
         plotCompetitorQueries,
-        legend to name=legendCompetitorsQueries,
+        legend to name=legendCompetitorsQueries,$additional
       ]
+      \\addplot[no marks,veryLightGrey,line width=2pt,forget plot,sharp plot] coordinates { (13,0) (13,100) };
       %% MULTIPLOT(name|ptitle|attr)
       %%   SELECT
       %%     MIN(bitsPerElement, 13) as x,
@@ -53,6 +56,6 @@ echo "% IMPORT-DATA competitors data/competitors.txt" > queries.tex
 echo "% IMPORT-DATA competitorNames data/__competitorNames.txt" >> queries.tex
 
 writePlots "trec-text.terms"
-writePlots "trec-title.terms"
-writePlots "uk-2007-05.urls"
+writePlots "trec-title.terms" "ylabel={},yticklabels={,,}"
+writePlots "uk-2007-05.urls" "ylabel={},yticklabels={,,}"
 
