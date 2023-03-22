e6a1520ebc7ae31b25536a38f8edc91e4d0a02e8
ByteHamster <info@bytehamster.com>
2023-03-22 15:34:28 +0100

    Do not sort sorted files again

--- cpp/benchmark/BenchmarkData.h
+++ cpp/benchmark/BenchmarkData.h
@@ -6,6 +6,7 @@
 #include <unordered_set>
 #include <algorithm>
 #include <fstream>
+#include <cstring>
 
 std::vector<std::string> loadStringFile(std::string &filename, size_t maxStrings) {
     std::cout<<"Loading input file"<<std::endl;
@@ -15,6 +16,11 @@ std::vector<std::string> loadStringFile(std::string &filename, size_t maxStrings
     const int MAX_LENGTH = 524288;
     char* line = new char[MAX_LENGTH];
     while (stream.getline(line, MAX_LENGTH)) {
+        if (!inputData.empty()) {
+            if (strcmp(inputData.back().c_str(), line) > 0) {
+                throw std::runtime_error("Not sorted or duplicate key");
+            }
+        }
         inputData.emplace_back(line);
         if (inputData.size() >= maxStrings) {
             break;
@@ -36,8 +42,12 @@ std::vector<uint64_t> loadInt64File(std::string &filename, size_t maxN) {
     std::vector<uint64_t> inputData(size);
     fileIn.read(reinterpret_cast<char *>(inputData.data()), size * sizeof(uint64_t));
     fileIn.close();
-    std::cout<<"Sorting input data"<<std::endl;
-    std::sort(inputData.begin(), inputData.end());
+    std::cout<<"Checking if input data is sorted"<<std::endl;
+    for (size_t i = 1; i < inputData.size(); i++) {
+        if (inputData.at(i) < inputData.at(i - 1)) {
+            throw std::runtime_error("Not sorted or duplicate key");
+        }
+    }
     std::cout<<"Loaded "<<inputData.size()<<" integers"<<std::endl;
     return inputData;
 }
@@ -52,14 +62,15 @@ std::vector<uint64_t> loadInt32File(std::string &filename, size_t maxN) {
     std::vector<uint32_t> inputData(size);
     fileIn.read(reinterpret_cast<char *>(inputData.data()), size * sizeof(uint32_t));
     fileIn.close();
-    std::cout<<"Sorting input data"<<std::endl;
-    std::sort(inputData.begin(), inputData.end());
     std::cout<<"Loaded "<<inputData.size()<<" integers"<<std::endl;
     std::cout<<"Converting to uint64"<<std::endl;
     std::vector<uint64_t> inputDataConverted;
     inputDataConverted.reserve(size);
-    for (uint32_t x : inputData) {
-        inputDataConverted.push_back(x);
+    for (size_t i = 0; i < inputData.size(); i++) {
+        inputDataConverted.push_back(inputData.at(i));
+        if (i > 0 && inputData.at(i) <= inputData.at(i - 1)) {
+            throw std::runtime_error("Not sorted or duplicate key");
+        }
     }
     return inputDataConverted;
 }
